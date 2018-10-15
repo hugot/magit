e752c3f0ef964737f6bf592fd6d99b1115f2cb90
Jonas Bernoulli <jonas@bernoul.li>
2018-10-15 12:16:00 +0200

    Define magit-commit as a transient command [*tmp]

--- lisp/magit-files.el
+++ lisp/magit-files.el
@@ -229,7 +229,7 @@ directory, while reading the FILENAME."
   :info-manual "(magit) Minor Mode for Buffers Visiting Files"
   [[("s" "Stage"      magit-stage-file)
     ("u" "Unstage"    magit-unstage-file)
-    ("c" "Commit"     magit-commit-popup)
+    ("c" "Commit"     magit-commit)
     ("e" "Edit line"  magit-edit-line-commit)]
    [("D" "Diff..."    magit-diff-buffer-file-popup)
     ("d" "Diff"       magit-diff-buffer-file)]
